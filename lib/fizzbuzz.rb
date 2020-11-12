799b445ea9225026c2d73e6ad312b87589dd543d
calavell <calavell@tcd.ie>
2020-11-12 13:43:26 +0000

    test added, pass 1 returns 1

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+class Integer
+  def fizzbuzz
+    '1'
+  end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,6 @@
+require 'fizzbuzz'
+describe 'fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(1.fizzbuzz).to eq '1'
+  end
+end
